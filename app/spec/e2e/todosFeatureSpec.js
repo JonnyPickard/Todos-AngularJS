4eef69a501a3373ed8e26e23d8fb656dab655f09
Jonny Pickard <jonathandpickard@gmail.com>
2016-04-29 14:28:29 +0100

    MongoDB locally setup, server.js configured

--- app/spec/e2e/todosFeatureSpec.js
+++ app/spec/e2e/todosFeatureSpec.js
@@ -1,6 +1,6 @@
-describe('angularjs homepage todo list', function() {
+describe('Todo list', function() {
   it('should add a todo', function() {
-    browser.get('https://angularjs.org');
+    browser.get('https://localhost:8080');
 
     element(by.model('todoList.todoText')).sendKeys('write first protractor test');
     element(by.css('[value="add"]')).click();
@@ -9,7 +9,6 @@ describe('angularjs homepage todo list', function() {
     expect(todoList.count()).toEqual(3);
     expect(todoList.get(2).getText()).toEqual('write first protractor test');
 
-    // You wrote your first test, cross it off the list
     todoList.get(2).element(by.css('input')).click();
     var completedAmount = element.all(by.css('.done-true'));
     expect(completedAmount.count()).toEqual(2);
